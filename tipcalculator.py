7d530caddf16f30a1fd730d9226976f4e88c5d63
taleenelhag <taleene@nycstudents.net>
2025-02-10 10:07:45 -0500

    commit

---
+++ tipcalculator.py
@@ -0,0 +1,27 @@
+#Strings are for representing characters, names, words etc.
+name = "Deivid"
+#integer represents whole numbers
+age: 14
+#Floats represents decimals
+wallet = 5.45
+#boolean represents true or false, used in evaluations 
+graduated = False
+
+def add(x,y):
+    print(x+y)
+#input asks the user a question and stores their response as a value
+bill = float(input("What was the bill?")) 
+#print(type(bill)) 
+add(40,bill): 
+
+#lists
+students = ["Joanna" , "Deivid: , "David" , "other David" , "Ethan"]
+#similar to saying for i in range(5): print (students[i])
+""" print(students[4])
+for student in students:
+    print(student) """
+moneys = [1,2,3,4,5,6] 
+total = 0
+for money in moneys 
+    total = total + money 
+        print(total)
