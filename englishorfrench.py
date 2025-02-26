7bdd458d36476028bd2cd05c2c983cc73a712f5e
taleenelhag <taleene@nycstudents.net>
2025-02-26 10:14:47 -0500

    commit

---
+++ englishorfrench.py
@@ -0,0 +1,16 @@
+""" language = input("Enter the text") 
+(0 < N < 10 000) = input
+def language(letters, N amount): 
+    if letters == True:
+        if letter t > s  
+            print("English")
+        if letter s > t
+            print("french")  """
+
+
+user_text = input("Enter text: ")
+check list ("t,T or s,S")
+    if "t,T" > "s,S"
+        print("English")
+for i in user_text:
+    print(i)
